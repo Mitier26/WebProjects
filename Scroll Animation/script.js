71e9826fe58aabddf841d10864edc0a4d163f336
Molkang <mitier26@gmail.com>
2024-02-29 09:49:20 +0900

    Scroll Animation

---
+++ Scroll Animation/script.js
@@ -0,0 +1,23 @@
+let text = document.querySelector("h2");
+let textString = text.textContent;
+let split = textString.split('');
+text.textContent = '';
+
+for(let i = 0; i < split.length; i++)
+{
+    text.innerHTML += "<span>" + split[i] + "</span>"
+}
+
+let tl = gsap.timeline();
+tl.from("span", {
+    y: -500,
+    opacity: 0,
+    scrollTrigger: {
+        pin: true,
+        scrub: 1,
+        trigger:"section",
+    },
+    stagger:{
+        amount: 2
+    }
+})
