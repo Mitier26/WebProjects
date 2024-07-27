ebcc43dd3451f95f1f89d36ad10766160b6ece3b
Molkang <mitier26@gmail.com>
2024-07-27 23:28:22 +0900

    잉크 효과
    https://cdnjs.com/libraries/gsap
    라는 외부 cdn을 사용했다

---
+++ 24_07_27_Ink Cousor/script.js
@@ -0,0 +1,22 @@
+let body = document.querySelector('body');
+body.onclick = function () {
+    body.classList.toggle('dark');
+}
+
+let cursor = document.querySelector('.cursor');
+for (let i = 0; i < 200; i++) {
+    let circle = document.createElement('div');
+    circle.classList.add('circle');
+    cursor.appendChild(circle);
+}
+
+document.body.addEventListener("mousemove", e => {
+    gsap.to(".circle", {
+        x: e.clientX,
+        y: e.clientY,
+        stagger: -0.0025,
+        scale: (i, target) => {
+            return 1 + (i * (2 / 200));
+        }
+    })
+})
