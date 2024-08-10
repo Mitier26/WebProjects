eeea9eedd5f5894b06c4ccd6355016ee65c0fc7e
Molkang <mitier26@gmail.com>
2024-08-10 21:20:16 +0900

    Animation Circle

---
+++ 24_08_10_Animation Circle/script.js
@@ -0,0 +1,15 @@
+let container = document.querySelector('.container');
+
+for (let j = 0; j < 4; j++) {
+    let loader = document.createElement('div');
+    loader.classList.add('loader');
+    loader.style.setProperty('--j', j);
+
+    for (let i = 1; i <= 20; i++) {
+        let span = document.createElement('span');
+        span.style.setProperty('--i', i);
+        loader.appendChild(span);
+    }
+
+    container.appendChild(loader);
+}
