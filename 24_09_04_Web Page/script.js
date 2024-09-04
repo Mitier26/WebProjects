f9558e7cb01bb26bded3b103fd732cc4e5fb9473
Molkang <mitier26@gmail.com>
2024-09-04 23:20:35 +0900

    웹 사이트

---
+++ 24_09_04_Web Page/script.js
@@ -0,0 +1,35 @@
+const dayNight = document.querySelector('.dayNight');
+const body = document.querySelector('body');
+
+dayNight.onclick = function () {
+    dayNight.classList.toggle('active');
+    body.classList.toggle('light');
+}
+
+const toggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+
+toggle.onclick = function () {
+    toggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
+const slides = document.querySelectorAll('.slides');
+const dots = document.querySelectorAll('.dot');
+
+function setActive(i) {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+        slides[i].classList.add('active');
+    })
+    dots.forEach((dot) => {
+        dot.classList.remove('active');
+        dots[i].classList.add('active');
+    })
+}
+
+for (let j = 0; j < dots.length; j++) {
+    dots[j].addEventListener('click', function () {
+        setActive(j);
+    })
+}
