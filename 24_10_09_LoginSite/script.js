e39bdf64926fe483d096a7d88d5d9763d035df50
Molkang <mitier26@gmail.com>
2024-10-09 14:30:27 +0900

    Sign up

---
+++ 24_10_09_LoginSite/script.js
@@ -0,0 +1,13 @@
+let wrapper = document.querySelector('.wrapper');
+let signupLink = document.querySelector('.signup-link');
+let signinLink = document.querySelector('.signin-link');
+
+signupLink.addEventListener('click', () => {
+  wrapper.classList.add("signinAnim");
+  wrapper.classList.remove('signupAnim');
+})
+
+signinLink.addEventListener('click', () => {
+  wrapper.classList.add('signupAnim');
+  wrapper.classList.remove("signinAnim");
+})
