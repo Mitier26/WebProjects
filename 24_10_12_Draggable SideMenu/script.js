5cf478f675a0187df2d42b39ed675f3ad9466c7f
Molkang <mitier26@gmail.com>
2024-10-12 22:05:52 +0900

    드래그 가능한 사이트 메뉴

---
+++ 24_10_12_Draggable SideMenu/script.js
@@ -0,0 +1,31 @@
+let sidebar = document.querySelector('.sidebar');
+let wrappers = document.querySelectorAll('.wrapper');
+
+sidebar.addEventListener('dragstart', (e) => {
+  e.dataTransfer.setData('idTxt', e.target.id);
+
+  console.log(e.dataTransfer.getData('idTxt'));
+});
+
+wrappers.forEach((wrapper) => {
+  wrapper.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    e.target.classList.add('drag');
+  })
+
+  wrapper.addEventListener('drop', (e) => {
+    e.preventDefault();
+    let data = e.dataTransfer.getData('idTxt');
+    console.log(data);
+    if (!wrapper.querySelector('.sidebar')) {
+      e.target.appendChild(document.querySelector("#" + data));
+    }
+
+    e.target.classList.remove('drag');
+  })
+
+  wrapper.addEventListener('dragleave', (e) => {
+    e.preventDefault();
+    e.target.classList.remove('drag');
+  })
+})
