73a1886079c3e2de971206df17b31c523eb2979c
Molkang <mitier26@gmail.com>
2024-10-19 22:13:37 +0900

    Slider Bar

---
+++ 24_10_19_Slide bar/script.js
@@ -0,0 +1,41 @@
+let slider = document.querySelector('.slider');
+let wrapper = document.querySelector('.wrapper');
+let nextBtn = document.querySelector('.next');
+let prevBtn = document.querySelector('.prev');
+
+let currentPos = 0;
+let scrollVal = 320;
+
+let maxScroll = -slider.offsetWidth + wrapper.offsetWidth;
+
+prevBtn.style.opacity = "0";
+
+nextBtn.addEventListener("click", () => {
+  scrollSlider(-1);
+})
+
+prevBtn.addEventListener('click', () => {
+  scrollSlider(1);
+})
+
+let scrollSlider = (val) => {
+  currentPos += val * scrollVal;
+
+  if (currentPos >= 0) {
+    currentPos = 0;
+    prevBtn.style.opacity = "0";
+  }
+  else {
+    prevBtn.style.opacity = "1";
+  }
+
+  if (currentPos <= maxScroll) {
+    currentPos = maxScroll;
+    nextBtn.style.opacity = "0";
+  }
+  else {
+    nextBtn.style.opacity = "1";
+  }
+
+  slider.style.left = currentPos + "px";
+}
